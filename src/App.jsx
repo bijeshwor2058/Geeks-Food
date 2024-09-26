a96970c854436623e5684d12fa7be4d72b8bb68a
Bijeshwor Sharma <bijeshwor2058@gmail.com>
2024-09-26 19:54:29 +0530

    Geeks Food

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Header from './common/Header'
+import Main_container from './Main_container/Main_container'
+import Footer from './Footer/Footer'
+
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Main_container />
+    <Footer/>
+    </>
+  )
+}
+export default App
